c188bf59142331fcfdacd0a9309350c0cc8aab99
Luke Olende <olendeluke2k@gmail.com>
2025-02-11 11:51:54 +0300

    Draw player/paddle 1

--- pong.js
+++ pong.js
@@ -1,12 +1,27 @@
-//Board
+// Board
 let board;
 let boardWidth = 500;
 let boardHeight = 500;
 let context;
+
+// Players / Paddle
+let playerWidth = 10;
+let playerHeight = 50;
+
+let player1 = {
+  x: 10,
+  y: boardHeight / 2,
+  width: playerWidth,
+  height: playerHeight,
+};
 
 window.onload = () => {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
   context = board.getContext("2d"); // used for drawing on the board
+
+  // draw initial player1 / paddle 1
+  context.fillStyle = "#67b3b5";
+  context.fillRect(player1.x, player1.y, player1.width, player1.height);
 };
